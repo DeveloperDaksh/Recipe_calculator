4ea7f406190e1916f612eb9f3ffdf6ed347553f9
Bhavyeshshah <bhavyeshshah259@gmail.com>
2021-11-28 12:01:54 +0530

    Sixth Commit

--- recipecostcaluclator-main/Recipe/urls.py
+++ recipecostcaluclator-main/Recipe/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.urls.conf import re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipeapp.urls')),
     path('recipe/', include('recipesandingredients.urls')),
     path('company/',include('company.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
